d9b763122ba990aaa4d51b1730bd887c722121ba
Mickael Delpech <mickael.delpech@gmail.com>
2019-08-12 16:43:15 +0200

    fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :sportifs
-  root to: 'pages#home'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20190812144106_devise_create_users.rb
+++ db/migrate/20190812144106_devise_create_users.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-class DeviseCreateSportifs < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :sportifs do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name
-      t.string :address
-      t.string :email
-      t.string :description
-      t.string :photo
-      t.string :offers
 
       ## Recoverable
       t.string   :reset_password_token
@@ -42,9 +36,9 @@ class DeviseCreateSportifs < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :sportifs, :email,                unique: true
-    add_index :sportifs, :reset_password_token, unique: true
-    # add_index :sportifs, :confirmation_token,   unique: true
-    # add_index :sportifs, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
