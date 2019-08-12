d6682255233822f2c4446d00c013d856d3ac3d60
James Arbacette <jamesarbacette@club-internet.fr>
2019-08-12 17:39:25 +0200

    Add sportif model

---
+++ db/migrate/20190812151739_create_sportifs.rb
@@ -0,0 +1,8 @@
+class CreateSportifs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sportifs do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_144106) do
+ActiveRecord::Schema.define(version: 2019_08_12_151739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "sportifs", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password", default: "", null: false
-    t.string "name"
-    t.string "address"
-    t.string "description"
-    t.string "photo"
-    t.string "offers"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_sportifs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_sportifs_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
